3861f147650a4164aaf0675e67a833b49f70904f
Rodrigo Coura <rodrigoc@corebridge.net>
2021-06-14 19:56:52 -0300

    First attempt of image button

---
+++ src/components/common/ImageButton.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-btn v-bind="$props"
+         class="q-img-btn"
+         v-bind:icon="$props.imgSrc"
+         v-on:click="toggle(), $props.cb($event)"
+         :class="{ disabled: isDisabled }"></q-btn>
+  <div class="q-img-btn-label"
+      :class="{ disabled: isDisabled }"><slot /></div>
+</template>
+
+<script lang="ts">
+import { Vue, Options, prop } from 'vue-class-component';
+import { QBtn } from 'quasar';
+
+class ImgButtonProps {
+  imgSrc = prop<string>({ required: true })
+  cb = function ($event: null) { return $event; }
+}
+
+@Options({
+  extends: QBtn,
+})
+export default class ImageButton extends Vue.with(ImgButtonProps) {
+  isDisabled = true;
+
+  toggle () {
+    this.isDisabled = !this.isDisabled;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .q-img-btn {
+    border-radius: 12px;
+    min-width: 340px;
+    border: 2px solid #F4538D;
+    box-sizing: border-box;
+    border-radius: 10px;
+  }
+
+  .q-img-btn-label {
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 22px;
+    line-height: 33px;
+    color: #000000;
+  }
+
+  .q-img-btn-label .disabled {
+    color: #F4538D;
+  }
+
+  .q-img-btn .disabled {
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.15);
+    border: none;
+  }
+</style>

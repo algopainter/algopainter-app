1c63c58974b539ddb1a81c07ead2a1aa5d9a7868
Gleisson de Assis <gleisson.assis@gmail.com>
2021-06-21 17:06:43 -0300

    Adding chart component and its dependencies

---
+++ src/components/charts/AuctionInfoChart.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <pie-chart
+      :labels="['Sellers', 'Creator', 'Investors', 'BidBack']"
+      :colors="['#F4538D', '#9E0039', '#D0004B', '#FFBCD4']"
+      :values="values"
+      :chart-data="chartData"
+      :options="options"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Options, prop } from 'vue-class-component';
+import PieChart from 'components/charts/PieChart.vue';
+
+class Props {
+  values = prop<number[]>({
+    required: true,
+  });
+}
+
+@Options({
+  components: {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    PieChart,
+  },
+})
+export default class AuctionInfoChart extends Vue.with(Props) {
+}
+</script>
---
+++ src/components/charts/PieChart.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <vue-apex-charts
+      type="pie"
+      :options="options"
+      :series="series"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Options, prop } from 'vue-class-component';
+import VueApexCharts from 'vue3-apexcharts';
+
+class Props {
+  labels = prop<string[]>({
+    required: true,
+  });
+
+  values = prop<number[]>({
+    required: true,
+  });
+
+  colors = prop<string[]>({
+    required: true,
+  });
+}
+
+@Options({
+  components: {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    VueApexCharts,
+  },
+})
+export default class PieChart extends Vue.with(Props) {
+  options = {
+    chart: {
+      id: 'vuechart-example',
+    },
+    labels: this.labels,
+    colors: this.colors,
+  };
+
+  series = this.values;
+}
+</script>

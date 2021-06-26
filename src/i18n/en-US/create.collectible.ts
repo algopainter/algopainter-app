09e179d27a31321a3e618af448eb4a8464769516
Rodrigo Coura <rodrigoc@corebridge.net>
2021-06-26 19:19:44 -0300

    Create upload component

--- src/i18n/en-US/create.collectible.ts
+++ src/i18n/en-US/create.collectible.ts
@@ -1,11 +1,12 @@
 export default {
   title: {
     main: 'Create Collectible',
-    sub: 'Select "Import your file" or "Create with Artist"',
+    sub: 'Select "Import your file" or "Create with an AlgoPainter"',
   },
   create: {
     importFile: 'Import your file',
-    createWithArtist: 'Create with Artist',
+    createWithArtist: 'Create with an AlgoPainter',
     preview: 'Preview',
+    uploadLabel: 'Upload File',
   },
 };

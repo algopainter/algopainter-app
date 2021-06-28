f9495baffbdc6e5fed5a78bef63fb05f5e4f6feb
Gustavo <guhmerces@hotmail.com>
2021-06-28 11:35:39 -0300

    Craete AuctionBidAvatar component

---
+++ src/components/auctions/auction/AuctionBidAvatar.vue
@@ -0,0 +1,49 @@
+<template>
+  <avatar-section
+    class="q-py-md"
+    :imageUrl="bid.user.profilePhotoUrl"
+  >
+    <template v-slot:title>
+      <div class="text-positive text-bold text-h6">
+        {{ $n(bid.price, 'currency') }}
+      </div>
+    </template>
+    <template v-slot:subTitle>
+      <div class="text-primary text-bold">
+        {{ bid.user.name }}
+      </div>
+    </template>
+    <template v-slot:description>
+      <div>
+        {{
+          $t('dashboard.auctionPage.bidBackPercentage', {
+            bidBackPercentage: $n(bid.art.bidBack, 'percent'),
+          })
+        }}
+      </div>
+    </template>
+  </avatar-section>
+</template>
+
+<script lang="ts">
+import { IBid } from 'src/models/IBid';
+import { Options, prop, Vue } from 'vue-class-component';
+import { PropType } from 'vue';
+import AvatarSection from 'src/components/AvatarSection.vue';
+
+class Props {
+  bid = prop({
+    type: Object as PropType<IBid>,
+    required: true,
+  });
+}
+
+@Options({
+  components: {
+    AvatarSection,
+  },
+})
+export default class AuctionBidAvatar extends Vue.with(Props) {
+
+}
+</script>

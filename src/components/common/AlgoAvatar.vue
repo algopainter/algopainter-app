3a92dbe9246ae4f4b80921a00a7747b3c1df0eda
Gustavo <guhmerces@hotmail.com>
2021-06-28 11:35:23 -0300

    Create AlgoAvatar component

---
+++ src/components/common/AlgoAvatar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="flex items-center">
+    <div class="items-center">
+      <q-item dense class="q-pa-none q-mr-xl">
+        <q-item-section side>
+          <q-avatar round size="56px">
+            <img :src="imageUrl" />
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label class="title q-mt-none">
+            <slot name="title">{{ title }}</slot>
+          </q-item-label>
+          <q-item-label class="subtitle" style="margin-top: 0px !important">
+            <slot name="subTitle">
+              {{ subTitle }}
+            </slot></q-item-label
+          >
+        </q-item-section>
+      </q-item>
+    </div>
+    <slot name="description">
+      <div class="text-bold">
+        {{ description }}
+      </div>
+    </slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, prop } from 'vue-class-component';
+
+class Props {
+  imageUrl = prop({
+    type: String,
+  });
+
+  title = prop({
+    type: String,
+  });
+
+  subTitle = prop({
+    type: String,
+  });
+
+  description = prop({
+    type: String,
+  });
+}
+
+export default class AvatarSection extends Vue.with(Props) {}
+</script>
+
+<style lang="scss" scope>
+.title {
+  line-height: 1.375rem;
+}
+.subtitle {
+  line-height: 1.3rem;
+  font-size: 1rem;
+}
+</style>

6f9de2aacb0633702ac8ed7edd3e4b701994acff
Fran Bernardo <62621670+FranBernardo@users.noreply.github.com>
2021-06-29 17:57:40 -0300

    create component iaArtist

---
+++ src/pages/collectible/IaArtist.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="col-4">
+    <q-img
+      class="img"
+      :src="img"
+    />
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Options, prop } from 'vue-class-component';
+class Props {
+  img = prop({
+    type: String,
+    required: true,
+  });
+
+  name = prop({
+    type: String,
+    required: true,
+  });
+}
+@Options({
+  components: {},
+})
+export default class IaArtist extends Vue.with(Props) {}
+</script>
+
+<style scoped>
+.img {
+  width: 100%;
+  max-width: 150px;
+  height: auto;
+  cursor: pointer;
+}
+</style>

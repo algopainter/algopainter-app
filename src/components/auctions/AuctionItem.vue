3d39058882845cf038c4b26ed455e55879f4561b
Bruno Arrais <brunoarraisbarretos@gmail.com>
2021-07-06 17:46:37 -0300

    Add animation heart button

--- src/components/auctions/AuctionItem.vue
+++ src/components/auctions/AuctionItem.vue
@@ -21,12 +21,33 @@
             name="mdi-dots-horizontal"
           />
         </div>
-        <div>
-          <q-icon
-            color="primary"
-            size="sm"
-            name="mdi-heart-outline"
-          />
+        <div class="col-12 col-md-1">
+          <div class="col-12 col-md-1">
+            <div class="icons text-center flex justify-center">
+              <div class="favorite">
+                <div>
+                  <q-icon
+                    color="primary"
+                    size="1.7rem"
+                    :name="
+                      isAuctionFavorite ? 'mdi-heart' : 'mdi-heart-outline'
+                    "
+                    :class="{
+                      press: isAuctionFavorite,
+                      shake: isAuctionFavorite,
+                    }"
+                    @click="favoriteAuction"
+                  />
+                  <div class="text-primary">
+                    {{ favoriteCounter }}
+                  </div>
+                  <span
+                    :class="{ press: isAuctionFavorite } q-pl-none"
+                  >{{ $t('dashboard.auctionPage.liked') }}</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -86,8 +107,30 @@ class Props {
   components: {
     AlgoButton,
   },
+  watch: {
+    isAuctionFavorite: ['incrementCounter', 'postFavoriteAuction'],
+  },
 })
 export default class AuctionItem extends Vue.with(Props) {
+  isAuctionFavorite: boolean = false;
+
+  favoriteAuction() {
+    this.isAuctionFavorite = !this.isAuctionFavorite;
+  }
+
+  incrementCounter() {
+    this.isAuctionFavorite ? this.favoriteCounter++ : this.favoriteCounter--;
+  }
+
+  postFavoriteAuction() {
+  // post http request
+    return true;
+  }
+
+  // FAKE DATA
+  favoriteCounter: number = parseInt(
+    (Math.random() * 100).toString(),
+  );
 }
 </script>
 
@@ -125,5 +168,55 @@ export default class AuctionItem extends Vue.with(Props) {
   .highest-bid {
     font-size: 1.1rem;
   }
+}
+
+.favorite {
+div {
+  height: 40px;
+  margin: 0 auto;
+  position: relative;
+}
+@keyframes fade {
+  0% {
+    color: rgba(255, 255, 255, 0);
+  }
+  50% {
+    color: $primary;
+  }
+  100% {
+    color: rgba(255, 255, 255, 0);
+  }
+}
+span {
+  position: absolute;
+  bottom: 70px;
+  left: 0;
+  right: 0;
+  visibility: hidden;
+  transition: 0.6s;
+  z-index: -2;
+  font-size: 3px;
+  color: transparent;
+  font-weight: 400;
+}
+span.press {
+  bottom: 35px;
+  font-size: 14px;
+  visibility: visible;
+  animation: fade 1s;
+}
+.shake {
+  animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+  transform: translate3d(0, 0, 0);
+  backface-visibility: hidden;
+  perspective: 1000px;
+}
+
+@keyframes shake {
+  40%,
+  60% {
+    transform: translate3d(0, -5px, 0);
+  }
+}
 }
 </style>

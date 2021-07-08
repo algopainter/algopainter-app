92e012c09e4744a6421d02cf8a5c5e52374058b3
Gustavo <guhmerces@hotmail.com>
2021-07-08 11:17:56 -0300

    Add getters to user store

--- src/store/user/getters.ts
+++ src/store/user/getters.ts
@@ -3,7 +3,15 @@ import { StateInterface } from '..';
 import { UserStateInterface } from './types';
 
 const getters: GetterTree<UserStateInterface, StateInterface> = {
-
+  account(state: UserStateInterface): string|undefined {
+    return state.account;
+  },
+  isConnected(state: UserStateInterface): boolean {
+    return state.isConnected;
+  },
+  error(state: UserStateInterface): Error|undefined {
+    return state.error;
+  },
 };
 
 export default getters;

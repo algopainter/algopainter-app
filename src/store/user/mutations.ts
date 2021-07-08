f3f602d772f924cb1b84146d959669080560a90b
Gustavo <guhmerces@hotmail.com>
2021-07-08 11:18:08 -0300

    Add mutations to user store

--- src/store/user/mutations.ts
+++ src/store/user/mutations.ts
@@ -2,7 +2,15 @@ import { MutationTree } from 'vuex';
 import { UserStateInterface } from './types';
 
 const mutation: MutationTree<UserStateInterface> = {
-
+  setIsConnected(state: UserStateInterface, payload: boolean) {
+    state.isConnected = payload;
+  },
+  setAccount(state: UserStateInterface, payload: string) {
+    state.account = payload;
+  },
+  setError(state: UserStateInterface, payload: Error) {
+    state.error = payload;
+  },
 };
 
 export default mutation;

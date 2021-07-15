e386f518a5a179bef8e3b3c52b8e89d3507aa5fb
Bruno Arrais <brunoarraisbarretos@gmail.com>
2021-07-15 10:37:42 -0300

    page responsive

--- src/pages/collectible/Preview.vue
+++ src/pages/collectible/Preview.vue
@@ -1,32 +1,34 @@
 <template>
-  <div :hidden="$q.screen.lt.md">
-    <div class="text-h6">
-      {{ $t('createCollectible.create.preview') }}
-    </div>
-    <div class="row text-h6 ap-preview-box justify-center items-center">
-      <div class="col q-mt-lg">
-        <p
-          v-if="imagePreview === null"
-          class="text-center"
-        >
-          {{ $t('createCollectible.create.previewText') }}
-        </p>
-        <div v-else>
-          <q-img
-            :src="imagePreview"
-            class="img"
-          />
+  <div class="col preview-size">
+    <div :hidden="$q.screen.lt.md">
+      <div class="text-h6">
+        {{ $t('createCollectible.create.preview') }}
+      </div>
+      <div class="row text-h6 ap-preview-box justify-center items-center">
+        <div class="col">
+          <p
+            v-if="imagePreview === null"
+            class="text-center"
+          >
+            {{ $t('createCollectible.create.previewText') }}
+          </p>
+          <div v-else>
+            <q-img
+              :src="imagePreview"
+              class="img"
+            />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  <div class="row justify-center q-py-md q-mt-md">
-    <algo-button
-      type="submit"
-      color="primary"
-      :label="$t('createCollectible.create.btnCreate')"
-      @click="OpenModal = true"
-    />
+    <div class="row justify-center q-py-md q-mt-md">
+      <algo-button
+        type="submit"
+        color="primary"
+        :label="$t('createCollectible.create.btnCreate')"
+        @click="OpenModal = true"
+      />
+    </div>
   </div>
   <mint-modal
     v-model="OpenModal"
@@ -58,17 +60,23 @@ OpenModal: boolean = false;
 
 <style lang="scss" scoped>
 .ap-preview-box {
-  max-width: 400px;
   padding: 25px 50px 25px 50px;
   border: 2px dashed #f4538d;
   box-sizing: border-box;
-  height: 450px;
-  width: 350px;
+  height: 100%;
+  min-height: 450px;
+  // width: 350px;
   border-radius: 10px;
 }
+
 .img{
-  width: 95%;
-  height: 20%;
+  width: 100%;
+  height: 100%;
+}
+
+.preview-size {
+  width: 100%;
+  height: 450px;
 }
 
 </style>

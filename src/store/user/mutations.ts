7a2c404b7007e1cc7f812282f825443bb9bd87f0
Gustavo <guhmerces@hotmail.com>
2021-07-19 03:14:06 -0300

    Improve function arg types in user store mutations

--- src/store/user/mutations.ts
+++ src/store/user/mutations.ts
@@ -5,10 +5,10 @@ const mutation: MutationTree<UserStateInterface> = {
   setIsConnected(state: UserStateInterface, payload: boolean) {
     state.isConnected = payload;
   },
-  setAccount(state: UserStateInterface, payload: string) {
+  setAccount(state: UserStateInterface, payload?: string) {
     state.account = payload;
   },
-  setError(state: UserStateInterface, payload: Error) {
+  setError(state: UserStateInterface, payload?: Error) {
     state.error = payload;
   },
   setNetworkInfo(state: UserStateInterface, payload?: NetworkInfo) {

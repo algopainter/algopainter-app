2ffb38f1717b1c4b091ed259a8b56823e4d12caf
Gustavo <guhmerces@hotmail.com>
2021-07-20 03:07:51 -0300

    Refactor connectToWallet() and  add IWeb3Provider
    Make code more clear and concise
    
    Use of IWeb3Provider, a web3 provider generic type for this application

--- src/store/user/actions.ts
+++ src/store/user/actions.ts
@@ -1,69 +1,88 @@
 import { ActionTree, Commit } from 'vuex';
 import { StateInterface } from '..';
-import { UserStateInterface, Ethereumish, ProviderRpcError } from './types';
+import { UserStateInterface, IWeb3Provider } from './types';
 import Web3 from 'web3';
 // import { provider as Provider } from 'web3-core';
+// import WalletConnectProvider from '@walletconnect/web3-provider';
 
 enum WalletEnum {
-  METAMASK,
-  WALLET_CONNECT,
+  METAMASK = 'metamask',
 }
+
+type Web3ProviderConstructor = {
+  new (provider: IWeb3Provider): Web3;
+};
 
 declare global {
   interface Window {
-    ethereum: Ethereumish;
+    ethereum: IWeb3Provider;
+    Web3: Web3ProviderConstructor;
     web3: Web3;
   }
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const getProvider = (wallet: WalletEnum): Ethereumish => {
-  return window.ethereum;
+const connectionFlows = {
+  metamask: async (): Promise<IWeb3Provider> => {
+    return new Promise((resolve, reject) => {
+      if (window.ethereum) {
+        resolve(window.ethereum);
+      } else {
+        reject('Could not find ethereum object');
+      }
+    });
+  },
 };
 
-const getAccountData = async(provider: Ethereumish): Promise<Array<string>> =>
-  provider.request({ method: 'eth_requestAccounts' });
-
-const commitUserAccountStore = (accounts: string[], commit: Commit) => {
-  commit('setIsConnected', true);
-  commit('setAccount', accounts[0]);
-};
-
-const clearUserAccountStore = (commit: Commit, error?: Error) => {
+const clear = (commit: Commit, error?: Error) => {
   commit('setIsConnected', false);
   commit('setAccount', null);
   commit('setError', error);
+  commit('setNetworkInfo', null);
 };
 
 const actions: ActionTree<UserStateInterface, StateInterface> = {
   async connectToWallet({ commit }, wallet: WalletEnum) {
-    const provider = getProvider(wallet);
+    const providerFlow = connectionFlows[wallet];
 
-    if (!provider) {
-      console.log('No wallet provider was found');
-      return;
+    if (!providerFlow) {
+      console.log(`Could not find the requested service: ${wallet}`);
     }
 
-    provider.on('accountsChanged', (accounts: Array<string>) => {
-      commitUserAccountStore(accounts, commit);
+    const provider = await providerFlow();
+
+    const commitUserData = async(commit: Commit) => {
+      const accounts = await provider.request({
+        method: 'eth_requestAccounts',
+      });
+      const networkInfo = {
+        id: await web3.eth.net.getId(),
+        type: await web3.eth.net.getNetworkType(),
+      };
+      commit('setIsConnected', true);
+      commit('setAccount', accounts[0]);
+      commit('setNetworkInfo', networkInfo);
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    provider.on('accountsChanged', (_accounts: Array<string>) => {
+      void commitUserData(commit);
     });
 
-    // networkChanged is DEPRECATED
     provider.on('chainChanged', () => {
       window.location.reload();
     });
 
-    provider.on('disconnect', (error: ProviderRpcError) => {
-      clearUserAccountStore(commit, error);
+    provider.on('disconnect', () => {
+      clear(commit);
     });
 
-    // window.web3 = new Web3(provider as Provider);
+    const web3 = new window.Web3(provider);
+    window.web3 = web3; // replaces deprecated web3 object for a stable one from cdn
 
     try {
-      const accounts: string[] = await getAccountData(provider);
-      commitUserAccountStore(accounts, commit);
+      void commitUserData(commit);
     } catch (error) {
-      clearUserAccountStore(commit, error);
+      clear(commit, error);
     }
   },
 };

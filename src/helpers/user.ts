b5e1fcfe6eeac2048fd633df375aad0fd1ed7b97
Gustavo <guhmerces@hotmail.com>
2021-07-23 03:24:38 -0300

    Add methods to user helpers file
    Create formatAccountBalance() and formatedAccount() methods

--- src/helpers/user.ts
+++ src/helpers/user.ts
@@ -6,8 +6,38 @@ const fetchAccountBalance = async(networkInfo: NetworkInfo, accountAddress: stri
   return (await algopainter.balanceOf(accountAddress)) as number;
 };
 
+/**
+ * input/output example
+ *
+ * Input: 6679.690202143615462628, 3
+ * Output: "6679.690..."
+ *
+ */
+const formatAccountBalance = (balance: number, decimalPlaces: number): string => {
+  const [integerPart, decimalPart] = balance.toString().split('.');
+  if (!decimalPart) {
+    return integerPart;
+  }
+  const slicedDecimal: string = decimalPart.slice(0, decimalPlaces);
+  return [integerPart, slicedDecimal + '...'].join('.');
+};
+
+/**
+ * input/output example
+ *
+ * Input: "0x38Ad0Ab1Dd2B9a64Dd6D75fB6D84a14893366DCD"
+ * Output: "0x38ad0ab1d...6dcd"
+ *
+ */
+const formatedAccount = (accountAddress: string) => {
+  const splited = accountAddress.split('');
+  return splited.slice(0, 11).join('') + '...' + splited.slice(-4).join('');
+};
+
 const helpers = {
   fetchAccountBalance,
+  formatAccountBalance,
+  formatedAccount,
 };
 
 export default helpers;

a2620bcbbd4f27dd1d1507766256969efbaa5320
Gustavo <guhmerces@hotmail.com>
2021-07-29 04:02:01 -0300

    Add favoriteArt to CollectionArtController

--- src/controllers/collectionArt/CollectionArtController.ts
+++ src/controllers/collectionArt/CollectionArtController.ts
@@ -1,5 +1,27 @@
 import BaseController from '../BaseController';
+import { nanoid } from 'nanoid';
 
 export default class CollectionArtController extends BaseController {
+  async favoriteArt(artId: string, address: string) {
+    try {
+      const data = {
+        imageId: artId,
+        salt: nanoid(),
+      };
+      const signature = await this.web3Helper.hashMessageAndAskForSignature(
+        data,
+        address
+      );
 
+      const request = this.requestToBeSigned(data, address);
+
+      const signedRequest = { ...request, signature };
+
+      const result = await this.post(`images/${artId}/like`, signedRequest);
+
+      return result.data as unknown;
+    } catch (error) {
+      return null;
+    }
+  }
 }

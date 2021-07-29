a9d187b54914e567de3a8f32860949709ad1f0fe
Gustavo <guhmerces@hotmail.com>
2021-07-29 03:56:54 -0300

    Add wrapper methods to BaseController

--- src/controllers/BaseController.ts
+++ src/controllers/BaseController.ts
@@ -1,14 +1,32 @@
 import { AxiosInstance } from 'axios';
 import { api } from '../boot/axios';
+import Web3Helper from 'src/helpers/web3Helper';
 
 export default class BaseController {
   api: AxiosInstance;
+  web3Helper: Web3Helper;
 
   constructor() {
     this.api = api;
+    this.web3Helper = new Web3Helper();
   }
 
   async get(path: string) {
     return await this.api.get(path);
+  }
+
+  async post(
+    path: string,
+    body: Record<string, unknown> = {},
+    options: Record<string, unknown> = {}
+  ) {
+    return await this.api.post(path, body, options);
+  }
+
+  async delete(
+    path: string,
+    body: Record<string, unknown> = {}
+  ) {
+    return await this.api.delete(path, body);
   }
 }

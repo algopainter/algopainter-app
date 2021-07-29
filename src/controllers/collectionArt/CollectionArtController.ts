a1fb664f46d208d4895396c0fd6991e4ba8a5f63
Gustavo <guhmerces@hotmail.com>
2021-07-29 04:02:12 -0300

    Add deleteFavoriteArt() to CollectionArtController

--- src/controllers/collectionArt/CollectionArtController.ts
+++ src/controllers/collectionArt/CollectionArtController.ts
@@ -24,4 +24,27 @@ export default class CollectionArtController extends BaseController {
       return null;
     }
   }
+
+  async deleteFavoriteArt(artId: string, address: string) {
+    try {
+      const data = {
+        imageId: artId,
+        salt: nanoid(),
+      };
+      const signature = await this.web3Helper.hashMessageAndAskForSignature(
+        data,
+        address
+      );
+
+      const request = this.requestToBeSigned(data, address);
+
+      const signedRequest = { ...request, signature };
+
+      const result = await this.delete(`images/${artId}/unlike`, signedRequest);
+
+      return result.data as unknown;
+    } catch (error) {
+      return null;
+    }
+  }
 }

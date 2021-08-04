da4188254736d4bb65ff54cc944ecef073d563cc
Gustavo <guhmerces@hotmail.com>
2021-08-04 02:13:58 -0300

    Change getUserProfile() return according to api

--- src/controllers/user/UserController.ts
+++ src/controllers/user/UserController.ts
@@ -1,6 +1,6 @@
 import { api } from 'src/boot/axios';
 import Result from 'src/core/Result';
-import { IApiResponse, IError } from 'src/core/types';
+import { IError } from 'src/core/types';
 import { IProfile } from 'src/models/IProfile';
 import BaseController from '../BaseController';
 
@@ -9,7 +9,7 @@ export default class UserController extends BaseController {
     try {
       const response = await api.get(`users/${account}`);
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      return Result.ok((response.data as IApiResponse<IProfile>).data);
+      return Result.ok(response.data as IProfile);
     } catch (error) {
       return Result.fail<string>((error as IError).message);
     }

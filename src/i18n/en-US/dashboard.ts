f10897363a4d6c795b1ba9cbe6101371a91bd1c6
Bruno Arrais <brunoarraisbarretos@gmail.com>
2021-08-19 14:48:12 -0300

    change text edit profile

--- src/i18n/en-US/dashboard.ts
+++ src/i18n/en-US/dashboard.ts
@@ -128,9 +128,9 @@ export default {
   editProfile: {
     title: 'Edit Profile',
     name: 'Name:',
-    email: 'Email: (public information for contact you)',
+    email: 'Email (public information to contact you):',
     custom: 'Custom URL',
-    webSite: 'Website ou portifolio',
+    webSite: 'Website or portfolio:',
     bio: 'Bio:',
     chooseFile: 'Choose File',
     sMedia: 'Social media',

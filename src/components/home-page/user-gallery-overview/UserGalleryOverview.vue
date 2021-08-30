c883123a665c059887815a1d50f0388a41f5d54c
Bruno Arrais <brunoarraisbarretos@gmail.com>
2021-08-30 16:38:02 -0300

    add counter in page usergallery

--- src/components/home-page/user-gallery-overview/UserGalleryOverview.vue
+++ src/components/home-page/user-gallery-overview/UserGalleryOverview.vue
@@ -1,19 +1,39 @@
 <template>
-  <div class="btn-container q-mx-auto flex justify-center items-center">
-    <algo-button
-      :label="$t('dashboard.homePage.gallery')"
-      outline
-      class="algo-button q-px-md q-ml-sm"
-      :color="currentBtnClicked === 1 ? 'primary' : 'grey-5' "
-      @click="showGalleryArts()"
-    />
-    <algo-button
-      :label="$t('dashboard.homePage.onSale')"
-      outline
-      class="algo-button q-px-md q-ml-sm"
-      :color="currentBtnClicked === 2 ? 'primary' : 'grey-5' "
-      @click="getOnSale()"
-    />
+  <div v-if="loadingGalleryArts === true">
+    <div class="btn-container q-mx-auto flex justify-center items-center">
+      <algo-button
+        :label="$t('dashboard.homePage.gallery')"
+        outline
+        class="algo-button q-px-md q-ml-sm"
+        :color="currentBtnClicked === 1 ? 'primary' : 'grey-5' "
+        @click="showGalleryArts()"
+      />
+      <algo-button
+        :label="$t('dashboard.homePage.onSale')"
+        outline
+        class="algo-button q-px-md q-ml-sm"
+        :color="currentBtnClicked === 2 ? 'primary' : 'grey-5' "
+        @click="getOnSale()"
+      />
+    </div>
+  </div>
+  <div v-else>
+    <div class="btn-container q-mx-auto flex justify-center items-center">
+      <algo-button
+        :label="$t('dashboard.homePage.gallery') + ' (' + contImg + ')'"
+        outline
+        class="algo-button q-px-md q-ml-sm"
+        :color="currentBtnClicked === 1 ? 'primary' : 'grey-5' "
+        @click="showGalleryArts()"
+      />
+      <algo-button
+        :label="$t('dashboard.homePage.onSale')"
+        outline
+        class="algo-button q-px-md q-ml-sm"
+        :color="currentBtnClicked === 2 ? 'primary' : 'grey-5' "
+        @click="getOnSale()"
+      />
+    </div>
   </div>
   <div
     class="row q-col-gutter-lg"
@@ -181,6 +201,9 @@ export default class UserGalleryOverview extends Vue {
   // Buttons
   currentBtnClicked: number = 1;
 
+  imgData: IMyGallery[] = [];
+  contImg: number = 0;
+
   favoriteClicked() {
     this.$emit('favoriteClicked');
   }
@@ -227,6 +250,7 @@ export default class UserGalleryOverview extends Vue {
       this.currentPage = page;
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
       const response = await api.get(`users/${this.$route.params.account}/images?page=${page}&perPage=9`);
+      const imgGet = await api.get(`users/${this.$route.params.account}/images`); // this.accountAddress
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       this.maxPage = response.data.pages as number;
       if (this.maxPage <= 15) {
@@ -236,6 +260,7 @@ export default class UserGalleryOverview extends Vue {
       }
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       this.galleryArts = response.data.data as [];
+      this.contImg = imgGet.headers['x-total-items'] as number;
       if (this.galleryArts.length === 0) {
         this.nullGalleryArts = true;
       } else {

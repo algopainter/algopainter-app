b92b9613d3d0e6ffcda1dd677c9f3ea86f77e814
Joao Paulo Martins <joao@kadro.io>
2021-09-09 17:14:03 -0300

    Add auction info tab

---
+++ src/components/auctions/auction/AuctionUserCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="highest-bid row">
+    <div class="col-auto q-pr-md row items-center">
+      <q-avatar
+        size="44px"
+        round
+      >
+        <img :src="user.avatar">
+      </q-avatar>
+    </div>
+    <div class="col row items-center user-type">
+      <div class="col-12">
+        {{ title }}
+      </div>
+      <div class="col-12 text-primary user-name">
+        {{ user.name }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Options, Prop } from 'vue-property-decorator';
+
+import { IUser } from 'src/models/IUser';
+
+@Options({
+  components: {
+  },
+})
+export default class AuctionUserCard extends Vue {
+  @Prop({ required: true }) user!: IUser;
+  @Prop({ required: true }) title!: string;
+}
+</script>
+
+<style lang="scss" scoped>
+.user-type {
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+
+.user-name {
+  font-weight: bold;
+  font-size: 1rem;
+}
+</style>
--- src/pages/dashboard/auctions/tabs/AuctionInfo.vue
+++ src/pages/dashboard/auctions/tabs/AuctionInfo.vue
@@ -1,8 +1,37 @@
 <template>
   <div class="row q-col-gutter-md">
     <div class="col-12">
-      <highest-bid-avatar
+      <highest-bid-card
         :bid="auction.highestBid"
+      />
+    </div>
+    <div class="col-12">
+      <auction-user-card
+        :user="auctionOwner"
+        title="Owner"
+      />
+    </div>
+    <div class="col-12">
+      <auction-user-card
+        :user="auctionCreator"
+        title="Creator"
+      />
+    </div>
+    <div class="col-12">
+      <q-separator />
+    </div>
+    <div class="col-12">
+      <auction-user-card
+        :user="collection"
+        title="Collection"
+      />
+    </div>
+    <div class="col-12">
+      <algo-button
+        class="full-width q-py-sm"
+        color="primary"
+        outline
+        label="Place a Bid"
       />
     </div>
   </div>
@@ -12,14 +41,39 @@
 import { Vue, Options, Prop } from 'vue-property-decorator';
 
 import { IAuctionItem } from 'src/models/IAuctionItem';
-import HighestBidAvatar from 'components/auctions/auction/HighestBidAvatar.vue';
+import HighestBidCard from 'components/auctions/auction/HighestBidCard.vue';
+import AuctionUserCard from 'components/auctions/auction/AuctionUserCard.vue';
+import AlgoButton from 'components/common/Button.vue';
 
 @Options({
   components: {
-    HighestBidAvatar,
+    AlgoButton,
+    HighestBidCard,
+    AuctionUserCard,
   },
 })
 export default class AuctionInfo extends Vue {
   @Prop({ required: true }) auction!: IAuctionItem;
+
+  get auctionOwner() {
+    return {
+      name: 'João',
+      avatar: 'http://localhost:8080/img/ALGOP.svg',
+    };
+  }
+
+  get auctionCreator() {
+    return {
+      name: 'Paulo',
+      avatar: 'http://localhost:8080/img/ALGOP.svg',
+    };
+  }
+
+  get collection() {
+    return {
+      name: 'Gwei',
+      avatar: 'http://localhost:8080/img/ALGOP.svg',
+    };
+  }
 }
 </script>

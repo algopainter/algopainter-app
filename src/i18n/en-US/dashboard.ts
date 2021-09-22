a3077431cd84916f6e054b346f2610d42a608683
fran <francielebernardo88@hotmail.com>
2021-09-22 14:22:44 -0300

    add text

--- src/i18n/en-US/dashboard.ts
+++ src/i18n/en-US/dashboard.ts
@@ -233,6 +233,7 @@ export default {
     clain: 'CLAIM',
     winBid: 'Winning bid',
     auctionEnd: 'Auction ended',
+    auctionText: 'Auction ends in ',
     noBids: 'No bids here yet...',
     goingOnce: 'Going once, going twice, sold!',
     outbid: 'Outbid',

a6e0b72eada3819e7196df9cf81d94c171166ceb
fran <francielebernardo88@hotmail.com>
2021-09-23 10:53:11 -0300

    add timer regressive

--- src/pages/dashboard/bids/BidsInfor.vue
+++ src/pages/dashboard/bids/BidsInfor.vue
@@ -87,39 +87,42 @@
               <div
                 class="text-bold"
               >
-                {{ monthExpirations }}  <span class="text-h6 text-bold"> {{ dayExpirations }} </span>
+                <!-- {{ dias }} {{ horas }} {{ minuReal }} {{ minutos }} {{ segundos }} -->
+                <!-- {{ monthExpirations }}  <span class="text-h6 text-bold"> {{ dayExpirations }}
+                  {{ $t('dashboard.bid.at') }} {{ hoursExpirations }} </span>
               </div>
-              <p> {{ yearExpirations }} </p>
-              <!-- <div
+              <p> {{ yearExpirations }} </p> -->
+                <div
 
-                class="row  justify-start time q-gutter-sm"
-              >
-                <div>
-                  <p class="text-bold">
-                    {{ Dias }}
-                  </p>
-                  <span> {{ $t('dashboard.bid.days') }} </span>
-                </div>
+                  class="row  justify-start time q-gutter-sm"
+                >
+                  <div>
+                    <p class="text-bold">
+                      {{ days }}
+                    </p>
+                    <span> {{ $t('dashboard.bid.days') }} </span>
+                  </div>
 
-                <div>
-                  <p class="text-bold">
-                    {{ Horas }}
-                  </p>
-                  <span>{{ $t('dashboard.bid.hours') }}</span>
+                  <div>
+                    <p class="text-bold">
+                      {{ hours }}
+                    </p>
+                    <span>{{ $t('dashboard.bid.hours') }}</span>
+                  </div>
+                  <div>
+                    <p class="text-bold">
+                      {{ minutes }}
+                    </p>
+                    <span>{{ $t('dashboard.bid.minis') }}</span>
+                  </div>
+                  <div>
+                    <p class="text-bold">
+                      {{ seconds }}
+                    </p>
+                    <span>{{ $t('dashboard.bid.seconds') }}</span>
+                  </div>
                 </div>
-                <div>
-                  <p class="text-bold">
-                    {{ Minutos }}
-                  </p>
-                  <span>{{ $t('dashboard.bid.minis') }}</span>
-                </div>
-                <div>
-                  <p class="text-bold">
-                    {{ Segundos }}
-                  </p>
-                  <span>{{ $t('dashboard.bid.seconds') }}</span>
-                </div>
-              </div> -->
+              </div>
             </div>
           </div>
         </div>
@@ -138,7 +141,7 @@ import { auctionCoins } from 'src/helpers/auctionCoins';
 import { IAuctionItem } from 'src/models/IAuctionItem';
 import { IBid } from 'src/models/IBid';
 // eslint-disable-next-line import/named
-import moment, { Duration } from 'moment';
+import moment from 'moment';
 import { Watch } from 'vue-property-decorator';
 import 'moment-duration-format';
 
@@ -161,27 +164,57 @@ class Props {
 
 export default class BidsInfor extends Vue.with(Props) {
 lastBidAuctions: IBid[] = [];
-dias: number = 0;
-segundos: number = 0;
-time!: Duration ;
+days: number = 0;
+hours: number = 0;
+minutes: number = 0;
+minuReal: number = 0;
+seconds: number = 0;
+time!: number ;
 monthExpirations: string = ''
 dayExpirations: string = ''
 yearExpirations: string = ''
+hoursExpirations!: string ;
 
  @Watch('now')
 onPropertyChanged() {
-  void this.getTime();
+  // void this.getTime();
 }
 
  mounted() {
-   void this.getTime();
+   // void this.getTime();
    void this.formatTime();
+   void this.countdown();
  }
 
  formatTime() {
    this.monthExpirations = moment(this.bidsAuctions.expirationDt).format('MMM');
    this.dayExpirations = moment(this.bidsAuctions.expirationDt).format('DD');
    this.yearExpirations = moment(this.bidsAuctions.expirationDt).format('YYYY');
+   this.hoursExpirations = moment(this.bidsAuctions.expirationDt).format('h:mm:ss');
+ }
+
+ countdown() {
+   const date = this.bidsAuctions.expirationDt;
+   const lack = (new Date(date).getTime() - new Date().getTime()) / 1000;
+   this.seconds = Math.round(lack % 60);
+   this.minutes = Math.round(lack / 60 % 60);
+   this.hours = Math.round(lack / 60 / 60 % 24);
+   this.days = Math.round(lack / 60 / 60 / 24);
+   setInterval(() => {
+     if (this.seconds === 0) {
+       this.seconds = 60;
+       this.minutes--;
+     }
+     if (this.minutes === 0) {
+       this.minutes = 60;
+       this.hours--;
+     }
+     if (this.hours === 0) {
+       this.hours = 24;
+       this.days--;
+     }
+     this.seconds--;
+   }, 1000);
  }
 
  dataMoment(index: string, format: string) {
@@ -200,39 +233,41 @@ onPropertyChanged() {
    return moment().isAfter(this.bidsAuctions.expirationDt);
  }
 
- getTime() {
-   const newEnded = moment(this.bidsAuctions.expirationDt);
-   const timeLeft = moment.duration(newEnded.diff(moment()));
-   this.time = timeLeft as unknown as Duration;
- }
+ //  getTime() {
+ //    const newEnded = moment(this.bidsAuctions.expirationDt);
+ //    const timeLeft = moment(newEnded.diff(moment()));
+ //    this.time = timeLeft as unknown as number;
+ //  }
 
- get Dias() {
-   if (this.time) {
-     return this.time.days();
-   }
-   return 0;
- }
+ //  get Dias() {
+ //    if (this.time) {
+ //      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+ //      return this.time.days();
+ //    }
+ //    return 0;
+ //  }
 
- get Horas() {
-   if (this.time) {
-     return this.time.hours();
-   }
-   return 0;
- }
+ //  get Horas() {
+ //    if (this.time) {
+ //      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+ //      return this.time.hours();
+ //    }
+ //    return 0;
+ //  }
 
- get Minutos() {
-   if (this.time) {
-     return this.time.minutes();
-   }
-   return 0;
- }
+ //  get Minutos() {
+ //    if (this.time) {
+ //      return this.time.minutes();
+ //    }
+ //    return 0;
+ //  }
 
- get Segundos() {
-   if (this.time) {
-     return this.time.seconds();
-   }
-   return 0;
- }
+ //  get Segundos() {
+ //    if (this.time) {
+ //      return this.time.seconds();
+ //    }
+ //    return 0;
+ //  }
 
  get coinDetails() {
    const coin = auctionCoins.find((coin) => {

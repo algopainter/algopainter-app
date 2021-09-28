cf79549d032fa1cfb579734bb965308bb5c7b955
brunoarrais <brunoarraisbarretos@gmail.com>
2021-09-28 11:01:17 -0300

    add function withdraw

--- src/eth/AlgoPainterAuctionSystemProxy.ts
+++ src/eth/AlgoPainterAuctionSystemProxy.ts
@@ -26,6 +26,7 @@ export default class AlgoPainterAuctionSystemProxy {
       bid(auctionId: number, amount: string): ContractSendMethod;
       cancelAuction(auctionId: number): ContractSendMethod;
       endAuction(auctionId: number): ContractSendMethod;
+      withdraw(auctionId: number): ContractSendMethod;
     };
   };
 
@@ -81,6 +82,12 @@ export default class AlgoPainterAuctionSystemProxy {
 
   endAuction(auctionId: number, from: string) {
     return this.smartContract.methods.endAuction(
+      auctionId,
+    ).send({ from });
+  }
+
+  withdraw(auctionId: number, from: string) {
+    return this.smartContract.methods.withdraw(
       auctionId,
     ).send({ from });
   }

88027076e5bdcc365af833b12ed64b08bfb99a55
fran <francielebernardo88@hotmail.com>
2022-01-04 10:36:50 -0300

    putting in the balance on the screen that the user will withdraw.

--- src/pages/dashboard/bids/BidsStatus.vue
+++ src/pages/dashboard/bids/BidsStatus.vue
@@ -39,7 +39,6 @@
       <div class="text-h6 text-bold text-center justify-center q-my-md">
         <div class="q-my-md">
           {{ $t('dashboard.bid.yourLastBid') }}
-          {{ amountToReturn }}
           <div class="row justify-center">
             {{ bidCorreting(MyHighBid) }}
             <div class="q-ml-sm">
@@ -50,7 +49,7 @@
         <div class="col q-gutter-sm">
           <algo-button v-if="endedAuction == false" size="lg" color="primary" :label="$t('dashboard.bid.bidAgain')" :to="`/auctions/${auctionItem._id}`" />
           <div v-if="hasReturn">
-            <algo-button size="lg" color="primary" :label="$t('dashboard.bid.removeBid')" @click="claimBid" />
+            <algo-button size="lg" color="primary" :label="$t('dashboard.bid.removeBid',{amountRemove: amountToReturn})" class="remove" @click="claimBid" />
           </div>
         </div>
       </div>
@@ -74,7 +73,7 @@
         </div>
         <div class="col q-gutter-sm">
           <div v-if="hasReturn">
-            <algo-button size="lg" color="primary" :label="$t('dashboard.bid.bidWithdraw' + auctionItem.returns[userAccount])" @click="claimBid" />
+            <algo-button size="lg" color="primary" :label="$t('dashboard.bid.bidWithdraw',{amountRemove: amountToReturn})" class="remove" @click="claimBid" />
           </div>
         </div>
       </div>
@@ -266,6 +265,9 @@ export default class BidsStatus extends Vue.with(Props) {
 </script>
 <style scoped>
  .box{
+    width: 250px;
+  }
+  .remove{
     width: 250px;
   }
 </style>

bb734ce64cffb30d4b9394c9e84abe51dc627ef2
Paulo Andrade <ph.and@hotmail.com>
2022-01-21 09:45:25 -0300

    auction coins addresses come from local variables

--- src/helpers/auctionCoins.ts
+++ src/helpers/auctionCoins.ts
@@ -1,29 +1,30 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 export const auctionCoins = [
   {
     value: '1',
     label: 'BTC',
-    tokenAddress: '0x6ce8da28e2f864420840cf74474eff5fd80e65b8',
+    tokenAddress: `${process.env.VUE_APP_BTC_TOKEN_ADDRESS}`,
     decimalPlaces: 18,
     img: '/images/BTC.svg',
   },
   {
     value: '2',
     label: 'BUSD',
-    tokenAddress: '0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee',
+    tokenAddress: `${process.env.VUE_APP_BUSD_TOKEN_ADDRESS}`,
     decimalPlaces: 18,
     img: '/images/BUSD.svg',
   },
   {
     value: '3',
     label: 'ALGOP',
-    tokenAddress: '0x01a9188076f1231df2215f67b6a63231fe5e293e',
+    tokenAddress: `${process.env.VUE_APP_ALGOP_TOKEN_ADDRESS}`,
     decimalPlaces: 18,
     img: '/images/ALGOP.svg',
   },
   {
     value: '4',
     label: 'DAI',
-    tokenAddress: '0xec5dcb5dbf4b114c9d0f65bccab49ec54f6a0867',
+    tokenAddress: `${process.env.VUE_APP_DAI_TOKEN_ADDRESS}`,
     decimalPlaces: 18,
     img: '/images/DAI.svg',
   },

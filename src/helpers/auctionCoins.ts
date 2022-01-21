9eca8839a885a330876e50295b2cf91a46e15cd1
Rodrigo Cour <rrcoura@gmail.com>
2022-01-21 10:13:39 -0300

    Removing uneeded formating string

--- src/helpers/auctionCoins.ts
+++ src/helpers/auctionCoins.ts
@@ -1,30 +1,29 @@
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
 export const auctionCoins = [
   {
     value: '1',
     label: 'BTC',
-    tokenAddress: `${process.env.VUE_APP_BTC_TOKEN_ADDRESS}`,
+    tokenAddress: process.env.VUE_APP_BTC_TOKEN_ADDRESS,
     decimalPlaces: 18,
     img: '/images/BTC.svg',
   },
   {
     value: '2',
     label: 'BUSD',
-    tokenAddress: `${process.env.VUE_APP_BUSD_TOKEN_ADDRESS}`,
+    tokenAddress: process.env.VUE_APP_BUSD_TOKEN_ADDRESS,
     decimalPlaces: 18,
     img: '/images/BUSD.svg',
   },
   {
     value: '3',
     label: 'ALGOP',
-    tokenAddress: `${process.env.VUE_APP_ALGOP_TOKEN_ADDRESS}`,
+    tokenAddress: process.env.VUE_APP_ALGOP_TOKEN_ADDRESS,
     decimalPlaces: 18,
     img: '/images/ALGOP.svg',
   },
   {
     value: '4',
     label: 'DAI',
-    tokenAddress: `${process.env.VUE_APP_DAI_TOKEN_ADDRESS}`,
+    tokenAddress: process.env.VUE_APP_DAI_TOKEN_ADDRESS,
     decimalPlaces: 18,
     img: '/images/DAI.svg',
   },

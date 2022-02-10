ada0cc27a8429a2f112047221287dce46e7a3c67
fran <francielebernardo88@hotmail.com>
2022-02-10 17:58:04 -0300

    mock parameter of expressions.

--- src/pages/dashboard/newpaint/NewPaintingLeftInfo.vue
+++ src/pages/dashboard/newpaint/NewPaintingLeftInfo.vue
@@ -42,6 +42,289 @@ export default class NewPaintingLeftInfo extends Vue.with(Props) {
   mockedParams() {
     this.params = [
       {
+        name: 'background',
+        label: 'Background',
+        dataType: 'string',
+        fieldType: 'Select',
+        options: [
+          {
+            value: '1',
+            label: 'Bitcoin',
+          },
+          {
+            value: '2',
+            label: 'Block Saturation',
+          },
+          {
+            value: '3',
+            label: 'Inner Circuit',
+          },
+          {
+            value: '4',
+            label: 'High Voltage',
+          },
+          {
+            value: '5',
+            label: 'Mars',
+          },
+        ],
+        min: 0,
+        max: 10,
+        defaultValue: 'Bitcoin'
+      },
+      {
+        name: 'biological sex',
+        label: 'Biological Sex',
+        dataType: 'string',
+        fieldType: 'Select',
+        options: [
+          {
+            value: '0',
+            label: 'Female',
+          },
+          {
+            value: '1',
+            label: 'Male',
+          },
+        ],
+        min: 0,
+        max: 10,
+        defaultValue: 'Female'
+      },
+      {
+        name: 'Expressions',
+        label: 'Expressions',
+        dataType: 'string',
+        fieldType: 'Select',
+        options: [
+          {
+            value: '0',
+            label: 'Neutral',
+          },
+          {
+            value: '1',
+            label: 'Happy',
+          },
+          {
+            value: '2',
+            label: 'Angry',
+          },
+          {
+            value: '3',
+            label: 'Disgusted',
+          },
+          {
+            value: '4',
+            label: 'Impressed',
+          },
+        ],
+        min: 0,
+        max: 10,
+        defaultValue: 'Angry'
+      },
+      {
+        name: 'skin',
+        label: 'Skin',
+        dataType: 'string',
+        fieldType: 'Select',
+        options: [
+          {
+            value: '1',
+            label: 'Space Code',
+          },
+          {
+            value: '2',
+            label: 'Deep Tracking',
+          },
+          {
+            value: '3',
+            label: 'Nebula Metrics',
+          },
+          {
+            value: '4',
+            label: 'Plasma',
+          },
+          {
+            value: '5',
+            label: 'Galaxy Node',
+          },
+        ],
+        min: 0,
+        max: 10,
+        defaultValue: 'Space Code'
+      },
+      {
+        name: 'use wiseframe',
+        label: 'Use Wiseframe',
+        dataType: 'string',
+        fieldType: 'Checkbox',
+        options: [
+          {
+            label: 'option label',
+            value: 'option value'
+          }
+        ],
+        min: 0,
+        max: 10,
+        defaultValue: true
+      },
+      {
+        name: 'wiseframe hue',
+        label: 'Wiseframe Hue',
+        dataType: 'string',
+        fieldType: 'Slider',
+        options: [
+          {
+            label: 'option label',
+            value: 'option value'
+          }
+        ],
+        min: 0,
+        max: 9,
+        defaultValue: 0
+      },
+      {
+        name: 'Use Wireframe Blend',
+        label: 'Use Wireframe Blend',
+        dataType: 'string',
+        fieldType: 'Checkbox',
+        options: [
+          {
+            label: 'option label',
+            value: 'option value'
+          }
+        ],
+        min: 0,
+        max: 10,
+        defaultValue: true
+      },
+      {
+        name: 'Wireframe Blend style',
+        label: 'Wireframe Blend Style',
+        dataType: 'string',
+        fieldType: 'Select',
+        options: [
+          {
+            value: '0',
+            label: 'Add',
+          },
+          {
+            value: '1',
+            label: 'Darken',
+          },
+          {
+            value: '2',
+            label: 'Difference',
+          },
+          {
+            value: '3',
+            label: 'Exclusion',
+          },
+          {
+            value: '4',
+            label: 'Hardlight',
+          },
+          {
+            value: '5',
+            label: 'Lighten',
+          },
+          {
+            value: '7',
+            label: 'Multiply',
+          },
+        ],
+        min: 0,
+        max: 10,
+        defaultValue: 'Add'
+      },
+      {
+        name: 'innercolor hue',
+        label: 'InnerColor Hue',
+        dataType: 'string',
+        fieldType: 'Slider',
+        options: [
+          {
+            label: 'option label',
+            value: 'option value'
+          }
+        ],
+        min: 0,
+        max: 9,
+        defaultValue: 0
+      },
+      {
+        name: 'magic',
+        label: 'Magic',
+        dataType: 'string',
+        fieldType: 'Select',
+        options: [
+          {
+            value: '1',
+            label: 'Fractal Perception',
+          },
+          {
+            value: '2',
+            label: 'Soul Link',
+          },
+          {
+            value: '3',
+            label: 'Magnetic Fields',
+          },
+          {
+            value: '4',
+            label: 'Z-Mesh',
+          },
+        ],
+        min: 0,
+        max: 10,
+        defaultValue: 'Fractal Perception'
+      },
+      {
+        name: 'overlay hue',
+        label: 'Overlay Hue',
+        dataType: 'string',
+        fieldType: 'Slider',
+        options: [
+          {
+            label: 'option label',
+            value: 'option value'
+          }
+        ],
+        min: 0,
+        max: 9,
+        defaultValue: 0
+      },
+      {
+        name: 'use shadow',
+        label: 'Use Shadow',
+        dataType: 'string',
+        fieldType: 'Checkbox',
+        options: [
+          {
+            label: 'option label',
+            value: 'option value'
+          }
+        ],
+        min: 0,
+        max: 9,
+        defaultValue: false
+      },
+      {
+        name: 'flip',
+        label: 'Flip',
+        dataType: 'string',
+        fieldType: 'Checkbox',
+        options: [
+          {
+            label: 'option label',
+            value: 'option value'
+          }
+        ],
+        min: 0,
+        max: 9,
+        defaultValue: false
+      },
+      /*{
         name: 'Chemist',
         label: 'Chemist name',
         dataType: 'string',
@@ -109,6 +392,7 @@ export default class NewPaintingLeftInfo extends Vue.with(Props) {
         max: 10,
         defaultValue: 5
       },
+      */
     ]
 
     this.params?.forEach((param, i) => {
